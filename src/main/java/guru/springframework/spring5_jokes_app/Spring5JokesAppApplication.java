4b4e500483271fb91cab1262523ae3de45cadeec
GU <badegowdausha@gmail.com>
2020-09-27 12:24:04 +0530

    Xml based config

--- src/main/java/guru/springframework/spring5_jokes_app/Spring5JokesAppApplication.java
+++ src/main/java/guru/springframework/spring5_jokes_app/Spring5JokesAppApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5_jokes_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config")
 public class Spring5JokesAppApplication {
 
 	public static void main(String[] args) {
